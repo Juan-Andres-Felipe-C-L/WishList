ba4d1b3b43d1c62f461b1f6a3d5e67c566c3703d
Juan Andrés Felipe Castro Londoño <juanandresfelipec@gmail.com>
2026-06-18 11:46:26 -0500

    Feat: Se suben avances del Service de la lista de deseos.

--- src/main/java/com/JAFCL/WishList/controller/WishListController.java
+++ src/main/java/com/JAFCL/WishList/controller/WishListController.java
@@ -1,13 +1,22 @@
 package com.JAFCL.WishList.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.JAFCL.WishList.dto.MessageResponseDTO;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.JAFCL.WishList.dto.RegisterRequestDTO;
+import com.JAFCL.WishList.service.WishListService;
+
 
 
 @RestController
@@ -15,5 +24,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class WishListController {
     
+    private final WishListService wishListService;
 
+    @PostMapping("/addProducts")
+    public MessageResponseDTO addProducts(@RequestBody List<RegisterRequestDTO> requestList) {
+        MessageResponseDTO response = wishListService.addProducts(requestList);        
+        return response;
+    }
+    
 }
